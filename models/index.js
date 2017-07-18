88edb7f05a2ac9c3fd074e0d68f3767a4a76c8e4
janeosaur <janewie@gmail.com>
2017-07-18 14:43:05 -0700

    adds mongoose connect to models/index

--- models/index.js
+++ models/index.js
@@ -1,2 +1,5 @@
+var databaseUrl = process.env.MONGODB_URI;
+mongoose.createConnection(databaseUrl || 'mongodb://localhost/wayfarer2-api')
+
 module.exports.Comment = require('./comments');
 module.exports.City = require('./cities')
