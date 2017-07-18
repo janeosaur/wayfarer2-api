6a0bccc8c7ec9eaf6025edc9711353d8e8e4f9d3
janeosaur <janewie@gmail.com>
2017-07-18 14:48:40 -0700

    mongoose.connect instead

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 var databaseUrl = process.env.MONGODB_URI;
-mongoose.createConnection(databaseUrl || 'mongodb://localhost/wayfarer2-api')
+mongoose.connect(databaseUrl || 'mongodb://localhost/wayfarer2-api')
 
 module.exports.Comment = require('./comments');
 module.exports.City = require('./cities')
